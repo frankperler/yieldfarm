e84f5378b5dd9829f6ad579fb780b85d17d8571c
Frank Perler <fperler@gmail.com>
2021-07-25 14:47:58 +0200

    added staking, unstaking functionalities and tests

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,4 +1,4 @@
-// "SPDX-License-Identifier: UNLICENSED" 
+// "SPDX-License-Identifier: UNLICENSED"
 pragma solidity ^0.8.6;
 
 import "./DaiToken.sol";
@@ -6,15 +6,63 @@ import "./TegToken.sol";
 
 contract TokenFarm {
 
-  string public name = "Tegrity Token Farm";
-  TegToken public tegToken;
-  DaiToken public daiToken;
+    string public name = "Tegrity Token Farm";
 
-  constructor (TegToken _tegToken, DaiToken _daiToken) {
-    tegToken = _tegToken;
-    daiToken = _daiToken;
-  }
+    address public owner;
+    TegToken public tegToken;
+    DaiToken public daiToken;
 
-  
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
 
+    constructor (TegToken _tegToken, DaiToken _daiToken) {
+        tegToken = _tegToken;
+        daiToken = _daiToken;
+        owner = msg.sender;
+    }
+
+    //1. Stake Tokens (deposit)
+    function stakeTokens(uint256 _amount) public {
+      //Amount staked cannot be smaller than 0
+      require(_amount > 0, 'amount cannot be 0');
+      // Transfer Mock Dai to this contract for staking
+      daiToken.transferFrom(msg.sender, address(this), _amount);
+      //update staking balance
+      stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount; 
+      // add user to stakers array "only" if they have not yet staked
+      if(!hasStaked[msg.sender]) {
+        stakers.push(msg.sender);
+      }
+      //update staking status
+      isStaking[msg.sender] = true;
+      hasStaked[msg.sender] = true;
+    }
+
+    //2. Unstaking Tokens (withdraw)
+    function unstakeTokens() public {
+      //fetch staking balance
+      uint balance = stakingBalance[msg.sender];
+      //require amounnt > 0
+      require(balance>0, 'staking balance cannot be 0 or lower');
+      //unstake dai tokens from farm to investor
+      daiToken.transfer(msg.sender, balance);
+      //reset staking balance
+      stakingBalance[msg.sender] = 0;
+      //update staking status
+      isStaking[msg.sender] = false;
+    }
+
+    //3. Issuing Teg Tokens to investor on a regular basis
+    function issueTokens() public {
+      require(msg.sender==owner, 'only owner can call this function');
+      for (uint i=0; i<stakers.length; i++) {
+        address recipient = stakers[i];
+        uint balance = stakingBalance[recipient];
+        if (balance > 0) {
+          tegToken.transfer(recipient, balance); // send exactly the same amount of tegToken than dai Token deposited
+        }
+      }
+    }
 }
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -57,4 +57,54 @@ contract('TokenFarm', ([owner, investor]) => {
     })
   })
 
+  describe('Farming tokens', async () => {
+    it('rewards investors for staking mDai tokens', async () => {
+      let result
+      //check investor balance before staking
+      result = await daiToken.balanceOf(investor)
+      assert.equal(result.toString(), tokens('100'), 'investor Mock Dai balance correct before staking')
+
+      //stake mock dai tokens
+      await daiToken.approve(tokenFarm.address, tokens('100'), {from: investor})
+      await tokenFarm.stakeTokens(tokens('100'), {from: investor})
+
+      //check staking result
+      result = await daiToken.balanceOf(investor)
+      assert.equal(result.toString(), tokens('0'), 'investor Mock Dai wallet balance correct after staking')
+
+      result = await tokenFarm.stakingBalance(investor)
+      assert.equal(result.toString(), tokens('100'), 'investor staking balance correct after staking')
+
+      result = await tokenFarm.isStaking(investor)
+      assert.equal(result.toString(), 'true', 'investor staking status correct after staking')
+
+      //issue tokens
+      await tokenFarm.issueTokens({from:owner})
+
+      //check balances after issuance
+      result = await tegToken.balanceOf(investor)
+      assert.equal(result.toString(), tokens('100'), 'investor Tegrity Token wallet balance correct after issuance')
+
+      //ensure owner only can issue tokens
+      await tokenFarm.issueTokens({from:investor}).should.be.rejected;
+
+      //unstake tokens
+      await tokenFarm.unstakeTokens({from: investor})
+
+      //check results after unstaking
+      result = await daiToken.balanceOf(investor)
+      assert.equal(result.toString(), tokens('100'), 'investor Mock Dai wallet balance is correct after unstaking')
+
+      result = await daiToken.balanceOf(tokenFarm.address)
+      assert.equal(result.toString(), tokens('0'), 'Token Farm Mock Dai balance is correct after unstaking')
+
+      result = await tokenFarm.stakingBalance(investor)
+      assert.equal(result.toString(), tokens('0'), 'investor staking balance is correct after unstaking')
+
+      result = await tokenFarm.isStaking(investor)
+      assert.equal(result.toString(), 'false', 'investor staking status is correct after unstaking')
+
+    })
+  })
+
 })
